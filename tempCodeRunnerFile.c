245e48158431ac2b86c2f370d348c0eb16664f39
unknown <harshmetkel24@gmail.com>
2022-04-19 19:11:31 +0530

    remoed unnecessary files

--- tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
- // FILE *fp = fopen("./courier_info.csv", "a+");
-    // if (fp == NULL)
-    // {
-    //     printf("Unable to open csv file of courier!!\n");
-    //     return;
-    // }
-    // fprintf(fp, "%s, %s, %s\n", current_courier.sender_name, current_courier.receiver_name, current_courier.receiver_no);
-    // printf("New data for courier is  added successfully!!\n");
-    // fclose(fp);
