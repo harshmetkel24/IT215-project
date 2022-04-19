6b5aa4f83d2bdfb710e1310a9f407c613606177b
unknown <harshmetkel24@gmail.com>
2022-04-19 11:09:50 +0530

    added all functions forward declarations to header files

--- courier_utils.h
+++ courier_utils.h
@@ -11,25 +11,6 @@ struct Courier
     int courier_status;
 };
 
-void addCourier(struct Courier current_courier)
-{
-    //  printf("*\n");
-    current_courier.courier_status = 1; // courier is in sending status
-    FILE *fp = fopen("./courier_info.csv", "a+");
-    if (fp == NULL)
-    {
-        printf("Unable to open csv file of courier!!\n");
-        return;
-    }
-    fprintf(fp, "%s, %s, %s, %s, %s, %s\n", current_courier.sender_name, current_courier.receiver_name, current_courier.receiver_no, current_courier.receiver_address, current_courier.couier_message, current_courier.attachment_type);
-    printf("New data for courier is  added successfully!!\n");
-    fclose(fp);
-}
-void cancelCourier(struct Courier current_courier)
-{
-    current_courier.courier_status = -1; // courier is cancelled
-}
-int check_status(struct Courier current_courier)
-{
-    return current_courier.courier_status;
-}
+void addCourier(struct Courier current_courier);
+void cancelCourier(struct Courier current_courier);
+int check_status(struct Courier current_courier);
